732f07bfcd3184d0c2ac5229e010aca476b65131
Nandu Jagdish <nandu.jagdish@tu-berlin.de>
2023-06-14 18:21:36 +0200

    changes by prof

--- move_on_line.py
+++ move_on_line.py
@@ -2,13 +2,17 @@ from robotic import ry
 import numpy as np
 import time
 
+ry.params_add({'Franka/Kp_freq': [30., 30., 30., 20., 10., 15., 10.],
+               'Franka/friction': np.array([0.8, 1.0, 0.8, 1.0, 0.9, 0.5, 0.4]),
+               'Franka/Kd_ratio': np.array([0.6, 0.6, 0.3, 0.3, 0.3, 0.3, 0.4])})
+
+
 C = ry.Config()
 C.addFile(ry.raiPath('../rai-robotModels/scenarios/pandaSingle.g'))
 C.view(False)
 
 input("Press Enter to home")
-bot = ry.BotOp(C, False
-               )
+bot = ry.BotOp(C, True)
 bot.home(C)
 #Wait for the robot to finish homing
 while bot.getTimeToEnd()>0:
@@ -20,8 +24,8 @@ start_pose = home_pose +[0.3,0,0]
 goal_pose = home_pose +[-0.3,0,0]
 komo = ry.KOMO()
 komo.setConfig(C, True)
-komo.setTiming(2., 1, 1, 2)
-komo.addControlObjective([], 2, 1e-0)
+komo.setTiming(2., 1, 1, 1)
+komo.addControlObjective([], 0, 1e-0)
 komo.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq);
 komo.addObjective([], ry.FS.jointLimits, [], ry.OT.ineq);
 komo.addObjective([], ry.FS.vectorZ, ['l_gripper'], ry.OT.eq, [1e1],[0,-1,0])
@@ -32,6 +36,12 @@ ret = ry.NLP_Solver() \
     .setProblem(komo.nlp()) \
     .setOptions( stopTolerance=1e-2, verbose=4 ) \
     .solve()
+    
+print("optimization result:", ret)
+komo.view(True, "optimization result")
+#while(komo.view_play(True, .5)):
+#	print("hi")
+
 path_to_start = komo.getPath() 
 
 #Move from home to start
@@ -40,17 +50,16 @@ bot.move(path_to_start,[10])
 while bot.getTimeToEnd()>0:
     bot.sync(C, .1)
 
-
 #Move from start to finish with interpolation
-steps = 5
+steps = 20
 komo = ry.KOMO()
 komo.setConfig(C, True)
 komo.setTiming(steps, 1, 1., 2)
-komo.addControlObjective([], 2, 1e-0)
+komo.addControlObjective([], 1, 1e0)
+komo.addControlObjective([], 2, 1e0)
 komo.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq);
 komo.addObjective([], ry.FS.jointLimits, [], ry.OT.ineq);
 komo.addObjective([], ry.FS.vectorZ, ['l_gripper'], ry.OT.eq, [1e1],[0,-1,0])
-
 
 for i in range(steps):
     i=i+1
@@ -61,16 +70,23 @@ ret = ry.NLP_Solver() \
     .setProblem(komo.nlp()) \
     .setOptions( stopTolerance=1e-2, verbose=4 ) \
     .solve()
-path = komo.getPath() 
+print("optimization result:", ret)
+komo.view(True, "optimization result")
+#while(komo.view_play(True, .5)):
+#	print("hi")
+
+path = komo.getPath()
 
 input("Press Enter to move to goal postion with interpolation")
+bot.setControllerWriteData(1)
 bot.move(path,[10])
 while bot.getTimeToEnd()>0:
     bot.sync(C, .1)
+bot.setControllerWriteData(0)
 
 input("Press Enter to home")
 #Move back to home
-bot.home()
+bot.home(C)
 #Wait for the robot to finish homing
 while bot.getTimeToEnd()>0:
     bot.sync(C, .1)
